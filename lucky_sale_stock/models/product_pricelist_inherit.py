22c9a8e544ad4eb375f4821308e6f1deeb76e636
FatmaSharkawy <fdaify@iskyev.com>
2019-02-17 17:25:17 +0200

    [UPD] onchange method to add product price depend on pricelist lines percentage

--- lucky_sale_stock/models/product_pricelist_inherit.py
+++ lucky_sale_stock/models/product_pricelist_inherit.py
@@ -6,9 +6,9 @@ from odoo.exceptions import ValidationError
 class ProductPricelist(models.Model):
     _inherit = 'product.pricelist'
 
-    qty_applied_on = fields.Selection([
-        ('in_stock', 'In Stock'), ('out_of_stock', 'Out Of Stock')],
-        string="Apply On", default='in_stock', required=True, )
+    qty_applied_on = fields.Selection(
+        [('in_stock', 'In Stock'), ('out_of_stock', 'Out Of Stock'), ('other', 'Other'), ],
+        string="Apply On", default='other', required=True, )
 
     updated_range = fields.Integer(string='Updated Range')
     product_pricelist_line_ids = fields.One2many('product.pricelist.line', 'pricelist_id', string='Pricelist Lines')
--- lucky_sale_stock/models/sale_order_inherit.py
+++ lucky_sale_stock/models/sale_order_inherit.py
@@ -7,16 +7,16 @@ from datetime import timedelta
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    pricelist_id = fields.Many2one('product.pricelist', string='Pricelist', related='order_id.pricelist_id')
-
     @api.multi
-    @api.onchange('product_id', 'pricelist_id')
-    def change_product_price_depend_on_qty(self):
+    @api.onchange('product_id')
+    def product_id_change(self):
+        res = super(SaleOrderLine, self).product_id_change()
         for line in self:
             min_product_vendor_price = 0
             last_product_purchase_price = 0
             product_percentage = 0
-            if line.product_id and line.order_id.pricelist_id.qty_applied_on:
+            pricelist_percentage = 0
+            if line.product_id and line.order_id.pricelist_id.qty_applied_on != 'other':
                 pricelist_updated_range = line.order_id.pricelist_id.updated_range
 
                 # last order point for product
@@ -36,5 +36,19 @@ class SaleOrderLine(models.Model):
                     min_product_vendor_price = min(
                         [supplier_info.price for supplier_info in product_supplierinfo_objects])
                 if min_product_vendor_price > 0:
-                    product_percentage = (
-                                             min_product_vendor_price - last_product_purchase_price) / min_product_vendor_price
+                    product_percentage = ((
+                                              min_product_vendor_price - last_product_purchase_price) / min_product_vendor_price) * 100
+
+                # conditions part for stock qty
+                for pricelist_line in line.order_id.pricelist_id.product_pricelist_line_ids:
+                    if (
+                                    pricelist_line.product_id == line.product_id or pricelist_line.categ_id == line.product_id.categ_id) and pricelist_line.percentage_from <= product_percentage <= pricelist_line.percentage_to:
+                        if pricelist_line.apply_sign == 'minus':
+                            pricelist_percentage = -1 * pricelist_line.apply_percentage
+                        elif pricelist_line.apply_sign == 'plus':
+                            pricelist_percentage = pricelist_line.apply_percentage
+                    break
+
+                line.price_unit = min_product_vendor_price + (pricelist_percentage / 100) * min_product_vendor_price
+            else:
+                return res
